713ff00fac87f541f8d04a4a2d853fd3f6e52811
Carlos Costa <30331609+capcosta@users.noreply.github.com>
2017-12-01 23:28:29 -0800

    Connected buttons to View Controller

--- LiaSays/ViewController.swift
+++ LiaSays/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-
+	@IBAction func redButton(_ sender: UIButton) {
+		print("red")
+	}
+	
+	@IBAction func greenButton(_ sender: UIButton) {
+		print("green")
+	}
+	
+	@IBAction func yellowButton(_ sender: UIButton) {
+		print("yellow")
+	}
+	
+	@IBAction func blueButton(_ sender: UIButton) {
+		print("blue")
+	}
+	
 }
 
