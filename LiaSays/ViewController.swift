e2480ad14daa0dfffc3f54674e4d4ed0879649a9
Carlos Costa <30331609+capcosta@users.noreply.github.com>
2017-12-03 18:53:55 -0800

    added comment

--- LiaSays/ViewController.swift
+++ LiaSays/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	
+	// Button controls
+	
 	@IBAction func redButton(_ sender: UIButton) {
 		print("red")
 	}
