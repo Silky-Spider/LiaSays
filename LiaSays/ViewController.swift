1ac399529531ab6558a5353ba46f3244f0c840fb
Carlos Costa <30331609+capcosta@users.noreply.github.com>
2017-12-06 00:17:15 -0800

    Made buttons nicer. Added count of number of times colour was tapped correctly.

--- LiaSays/ViewController.swift
+++ LiaSays/ViewController.swift
@@ -11,43 +11,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	var colour = arc4random()
-	var level = 0
+	let maxSequence = 100
+	var level = 2
+	var sequence = [Int]()
+	var numberOfColours = 0
+	var count = 0
 	
-	@IBOutlet weak var levelNumber: UILabel!
+	@IBOutlet weak var levelNumberLabel: UILabel!
+	@IBOutlet weak var countLabel: UILabel!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		numberOfColours = level * 2
+		levelNumberLabel.text = String(level)
+		createSequence(lengthOf: maxSequence)
+		print("\(sequence)")
+		count = 0
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-	func updateLevel(_ amount: Int = 1) {
-		level += amount
-		levelNumber.text = String(level)
+	func createSequence(lengthOf: Int) {
+		for _ in 0..<lengthOf {
+			sequence.append(Int(arc4random_uniform(UInt32(numberOfColours))))
+		}
 	}
+	
+	func updateCount() {
+		count += 1
+		countLabel.text = String(count)
+	}
+
+	
 	// Button controls
 	
 	@IBAction func redButton(_ sender: UIButton) {
 		print("red")
-		updateLevel()
+		updateCount()
 		playSound(redBeep)
 	}
 	
 	@IBAction func greenButton(_ sender: UIButton) {
 		print("green")
-		updateLevel()
+		updateCount()
 		playSound(greenBeep)
 	}
 	
 	@IBAction func yellowButton(_ sender: UIButton) {
 		print("yellow")
-		updateLevel()
+		updateCount()
 		playSound(yellowBeep)
 	}
 	
 	@IBAction func blueButton(_ sender: UIButton) {
 		print("blue")
-		updateLevel()
+		updateCount()
 		playSound(blueBeep)
 	}
 	
